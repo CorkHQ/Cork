4ce3f7e2b64dfc0f29e8227e458fc9401b21d547
Bugadinho <miguelriechi_windows8@outlook.com>
2023-04-26 22:01:59 -0300

    Added utils

---
+++ cork/utils.py
@@ -0,0 +1,9 @@
+def deep_merge(source, destination):
+    for key, value in source.items():
+        if isinstance(value, dict):
+            node = destination.setdefault(key, {})
+            deep_merge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
