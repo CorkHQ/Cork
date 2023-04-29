fb19e56f6b37df42457ca707e7bb51f5aa5ac2c7
Bugadinho <miguelriechi_windows8@outlook.com>
2023-04-28 21:16:58 -0300

    Replaced spinner with bar

--- cork/splash.py
+++ cork/splash.py
@@ -5,7 +5,7 @@ gi.require_version("Gtk", "3.0")
 gi.require_version('GdkPixbuf', '2.0')
 
 from gi.repository.GdkPixbuf import Pixbuf
-from gi.repository import Gtk
+from gi.repository import GLib, Gtk
 
 class GtkCorkSplash(Gtk.Window):
     def __init__(self, icon):
@@ -24,8 +24,7 @@ class GtkCorkSplash(Gtk.Window):
             Gtk.IconTheme.get_default().load_icon(icon, 112, 0))
         self.image.set_hexpand(True)
 
-        self.spinner = Gtk.Spinner()
-        self.spinner.set_hexpand(True)
+        self.progressbar = Gtk.ProgressBar()
 
         self.label = Gtk.Label(label="")
         self.label.set_hexpand(True)
@@ -34,19 +33,23 @@ class GtkCorkSplash(Gtk.Window):
 
         self.grid.attach(self.image, 1, 0, 1, 3)
         self.grid.attach_next_to(
-            self.spinner, self.image, Gtk.PositionType.BOTTOM, 1, 1
+            self.progressbar, self.image, Gtk.PositionType.BOTTOM, 1, 1
         )
         self.grid.attach_next_to(
-            self.label, self.spinner, Gtk.PositionType.BOTTOM, 1, 1
+            self.label, self.image, Gtk.PositionType.BOTTOM, 1, 1
         )
         self.grid.set_row_homogeneous(True)
 
         self.add(self.grid)
-        self.spinner.start()
         self.show_all()
+        self.timeout_id = GLib.timeout_add(50, self.on_timeout, None)
     
     def set_text(self, text):
         self.label.set_text(text)
+    
+    def on_timeout(self, user_data):
+        self.progressbar.pulse()
+        return True
 
 class CorkSplash():
     def __init__(self):
@@ -57,7 +60,7 @@ class CorkSplash():
         def gtk_function():
             Gtk.main()
         
-        self.gtk_thread = threading.Thread(target=gtk_function)
+        self.gtk_thread = threading.Thread(target=gtk_function, daemon=True)
         self.gtk_thread.start()
     
     def set_text(self, text):
