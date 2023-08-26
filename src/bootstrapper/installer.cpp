2e86299a331fa36b5108729b04434b05fb75a18e
Bugadinho <miguelriechi_windows8@outlook.com>
2023-08-25 23:06:14 -0300

    Use libzippp instead of elzip

--- src/bootstrapper/installer.cpp
+++ src/bootstrapper/installer.cpp
@@ -1,7 +1,8 @@
+#include <vector>
 #include <iostream>
 #include <filesystem>
 #include <cpr/cpr.h>
-#include <elzip/elzip.hpp>
+#include <libzippp.h>
 #include "../roblox/cdn.hpp"
 #include "../roblox/packages.hpp"
 
@@ -28,19 +29,51 @@ namespace cork::bootstrapper {
             std::cout << package.name << std::endl;
 
             fs::path filePath = temporaryPath / package.name;
-            std::ofstream ofstream = std::ofstream(filePath, std::ios::binary);
+            std::ofstream zipStream = std::ofstream(filePath, std::ios::binary);
 
-            cpr::Response response = cpr::Download(ofstream, cpr::Url{package.url});
-            ofstream.close();
+            cpr::Response response = cpr::Download(zipStream, cpr::Url{package.url});
+            zipStream.close();
 
-            elz::extractZip(filePath, versionPath / package.target);
+            libzippp::ZipArchive zipFile(filePath);
+            zipFile.open(libzippp::ZipArchive::ReadOnly);
+
+            std::vector<libzippp::ZipEntry> entries = zipFile.getEntries();
+            std::vector<libzippp::ZipEntry>::iterator it;
+
+            for (it=entries.begin() ; it!=entries.end(); ++it) {
+                libzippp::ZipEntry entry = *it;
+                std::string name = entry.getName();
+
+                std::replace( name.begin(), name.end(), '\\', '/');
+                
+                int numberSlashes = 0;
+
+                for (char c : name) {
+                    if (c == '/') {
+                        numberSlashes += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                name.erase(0, numberSlashes);
+
+                fs::path targetPath = versionPath / package.target / name;
+                fs::create_directories(targetPath.parent_path());
+
+                std::ofstream fileStream(targetPath);
+                entry.readContent(fileStream);
+                fileStream.close();
+            }
+
+            zipFile.close();
 
             std::cout << "Done!" << std::endl;
         }
 
-        std::ofstream ofstream = std::ofstream(versionPath / "AppSettings.xml");
-        ofstream << appSettings;
-        ofstream.close();
+        std::ofstream appStream = std::ofstream(versionPath / "AppSettings.xml");
+        appStream << appSettings;
+        appStream.close();
 
         fs::remove_all(temporaryPath);
     }
