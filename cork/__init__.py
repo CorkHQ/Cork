7fddd5700159fecac3b85b9c117689b07b388a3d
Bugadinho <miguelriechi_windows8@outlook.com>
2023-09-06 01:37:03 -0300

    Goodbye, Alcântara

--- cork/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "1.0.2"
-__codename__ = "Alcântara"
--- cork/utils.py
+++
@@ -1,9 +0,0 @@
-def deep_merge(source, destination):
-    for key, value in source.items():
-        if isinstance(value, dict):
-            node = destination.setdefault(key, {})
-            deep_merge(value, node)
-        else:
-            destination[key] = value
-
-    return destination
