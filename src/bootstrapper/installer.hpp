8372450ec7c1f5d831fd237dc07641c66df2e1c9
Bugadinho <miguelriechi_windows8@outlook.com>
2023-09-12 22:28:10 -0300

    Fix fs

--- src/bootstrapper/installer.hpp
+++ src/bootstrapper/installer.hpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace cork::bootstrapper {
     void Install(std::string versionType, std::string version, std::string versionChannel, std::string versionDirectory, fs::path temporaryDirectory = "");
