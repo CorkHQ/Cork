29c3e442e2864ba83cdc82b5015082b47b5f15da
Bugadinho <miguelriechi_windows8@outlook.com>
2023-09-13 11:51:14 -0300

    Verify file checksum with OpenSSL

--- src/bootstrapper/installer.cpp
+++ src/bootstrapper/installer.cpp
@@ -3,6 +3,8 @@
 #include <stdexcept>
 #include <filesystem>
 #include <cpr/cpr.h>
+#include <openssl/md5.h>
+#include <openssl/evp.h>
 #ifdef LOCAL_LIBZIPPP
 #include <libzippp.h>
 #else
@@ -18,6 +20,17 @@ namespace cr = cork::roblox;
 namespace fs = std::filesystem;
 
 std::string appSettings = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<Settings>\r\n        <ContentFolder>content</ContentFolder>\r\n        <BaseUrl>http://www.roblox.com</BaseUrl>\r\n</Settings>\r\n";
+
+std::string hexToString(const uint8_t *data, int len)
+{
+    std::stringstream ss;
+    ss << std::hex;
+
+    for( int i(0) ; i < len; ++i )
+        ss << std::setw(2) << std::setfill('0') << (int)data[i];
+
+    return ss.str();
+}
 
 namespace cork::bootstrapper {
     void DisplayProgress(int installedPackages, int downloadedPackages, int totalPackages) {
@@ -52,6 +65,7 @@ namespace cork::bootstrapper {
         for (cr::package package : packages) {
             fs::path filePath = temporaryPath / package.name;
             std::ofstream zipStream = std::ofstream(filePath, std::ios::binary);
+            bool zipStreamOpen = true;
 
             int attempts = 0;
             bool success = false;
@@ -65,23 +79,57 @@ namespace cork::bootstrapper {
                         throw std::runtime_error("Server responded with status code of not 200!");
                     }
 
+                    zipStream.close();
+                    zipStreamOpen = false;
+
+                    std::ifstream inputStream(filePath, std::ios::binary);
+
+                    EVP_MD_CTX* md5Context = EVP_MD_CTX_new();
+                    EVP_MD_CTX_init(md5Context);
+                    EVP_DigestInit_ex(md5Context, EVP_md5(), nullptr);
+                    const size_t bufferSize = 4096;
+                    char buffer[bufferSize];
+                    while (!inputStream.eof()) {
+                        inputStream.read(buffer, bufferSize);
+                        EVP_DigestUpdate(md5Context, buffer, inputStream.gcount());
+                    }
+                    std::array<uint8_t, 16> result;
+                    EVP_DigestFinal_ex(md5Context, result.data(), nullptr);
+                    inputStream.close();
+                    EVP_MD_CTX_free(md5Context);
+
+                    std::string md5 = hexToString(result.data(), result.size());
+
+                    if (md5 != package.checksum) {
+                        throw std::runtime_error("Checksum incorrect!");
+                    }
+
                     success = true;
                 }
                 catch (std::exception &e) {
                     BOOST_LOG_TRIVIAL(warning) << "failed to download " << package.name << ": " << e.what();
 
-                    zipStream.clear();
-                    zipStream.seekp(std::ios::beg);
+                    if (zipStreamOpen == true) {
+                        zipStream.close();
+                        zipStreamOpen = false;
+                    }
+
+                    zipStream = std::ofstream(filePath, std::ios::binary);
+                    zipStreamOpen = true;
 
                     attempts++;
                 }
             }
 
             if (success == false) {
+                if (zipStreamOpen == true) {
+                    zipStream.close();
+                    zipStreamOpen = false;
+                }
+
                 throw std::runtime_error("Failed to download file!");
             }
 
-            zipStream.close();
             BOOST_LOG_TRIVIAL(trace) << "downloaded " << package.name << "!";
 
             downloadedPackages++;
